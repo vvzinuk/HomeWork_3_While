289968b25cfc07e44d21eeed2acea984cb442aaa
vvzinuk <43749479+vvzinuk@users.noreply.github.com>
2019-03-06 00:31:20 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main{
     public static void main (String[]args){
-        Drawer draw = new Drawer(" *");
-        draw.drawFirst();
-        draw.drawSecond();
-        draw.drawThird();
-        draw.drawFourth();
-        draw.drawFifth();
+        Drawer drawer = new Drawer(" *");
+        drawer.drawFirst();
+        drawer.drawSecond();
+        drawer.drawThird();
+        drawer.drawFourth();
+        drawer.drawFifth();
     }
 
 }
